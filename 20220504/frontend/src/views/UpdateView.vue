1c7e777168cb798b29917837196541cf80836d33
unknown <ecof1911@gmail.com>
2022-05-04 09:59:35 +0900

    서버에서 put, delete 사용

---
+++ 20220504/frontend/src/views/UpdateView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>글을 수정하는 곳입니다</h1>
+        <input type="text" v-model="title"> <br>
+        <textarea cols="30" rows="10" v-model='memo'></textarea> <br>
+        <button @click="upadatememo">제출</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            title :'',
+            memo : ''
+        }
+    },
+    created() {
+      this.$http.get('/api/memo/'+this.$route.params.id)
+        .then((res)=>{
+          this.title = res.data.title
+          this.memo = res.data.memo
+        })
+    },
+    methods : {
+
+        // axios를 이용해 제출 버튼을 눌렀을때 값을 보내줌 - post
+        upadatememo() {
+            this.$http.put('/api/memo/'+this.$route.params.id,{
+                data : {
+                    title :this.title,
+                    memo : this.memo
+                }
+            }).then((response)=>{
+                console.log(response.data)
+            });
+            this.$router.push('/')
+        }
+    }
+}
+</script>
